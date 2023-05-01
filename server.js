18082429a59313835bb4815e6da05a307ec1fdce
Hank Richter <hankrichter97@gmail.com>
2023-05-01 11:50:37 -0400

    Cleaned up the server file with unneeded code.

--- server.js
+++ server.js
@@ -1,12 +1,5 @@
-const express = require("express");
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
-
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
 const db = mysql.createConnection(
   {
@@ -57,10 +50,3 @@ const init = () =>{
   })
 };
 
-app.use((req, res) => {
-  res.status(404).end();
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
