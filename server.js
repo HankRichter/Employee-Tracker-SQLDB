dd238801a7a7f352a4f0e79926800e9de7538c54
Hank Richter <hankrichter97@gmail.com>
2023-05-01 11:59:02 -0400

    Undoing previous commit.

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
+const express = require("express");
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 const db = mysql.createConnection(
   {
@@ -50,3 +57,10 @@ const init = () =>{
   })
 };
 
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
