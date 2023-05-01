5df8a31360b29d79895de881c7e68225ba44344d
Hank Richter <hankrichter97@gmail.com>
2023-05-01 11:11:01 -0400

    Added init function and the switch case based on user selection.

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const inquirer = require("inquirer");
 const mysql = require("mysql2");
 
 const PORT = process.env.PORT || 3001;
@@ -16,6 +17,45 @@ const db = mysql.createConnection(
   },
   console.log(`Connected the the job database.`)
 );
+
+const init = () =>{
+    inquirer.prompt([
+      {
+      type: "list",
+      name: "Main Menu",
+      message: 'What would you like to do?',
+      choices: [
+      "View All Employees",
+      "Add Employee",
+      "Update employee role",
+      "View all roles",
+      "Add role",
+      "View all departments",
+      "Add department",
+      "Quit"]
+    }
+  ]).then(selection =>{
+    switch (selection.int) {
+    case "View All Employees":
+        break;
+    case "Add Employee":
+      break;
+    case "Update employee role":
+      break;
+    case "View all roles":
+      break;
+    case "Add role":
+      break;
+    case "View all departments":
+      break;
+    case "Add department":
+      break;
+    case "Quit": 
+      default:
+        break;
+    }
+  })
+};
 
 app.use((req, res) => {
   res.status(404).end();
