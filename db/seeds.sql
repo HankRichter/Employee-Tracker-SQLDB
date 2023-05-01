f5351943a8dd6721b612ed3226e6fd9eb0b30818
Hank Richter <120127039+HankRichter@users.noreply.github.com>
2023-04-30 20:18:05 -0400

    Updated file to clear errors

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,7 +5,7 @@ VALUES
 ("Legal"),
 ("Sales");
 
-INSERT INTO roles (department_id, role_title, role_salary)
+INSERT INTO roles (departments_id, title, salary)
 VALUES 
 (4, "Sales Lead", 100000),
 (4,"Salesperson",80000),
@@ -16,13 +16,13 @@ VALUES
 (3,"Legal Team Lead",250000),
 (3, "Lawyer",190000);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
+INSERT INTO employees (first_name, last_name, roles_id, manager_id)
 VALUES 
-("John", "Doe", 1),
+("John", "Doe", 1, NULL),
 ("Mike","Chan", 2, 1),
-("Ashley", "Rodriguez", 3),
+("Ashley", "Rodriguez", 3, NULL),
 ("Kevin", "Tupik", 4, 3),
-("Kunal", "Singh", 5),
+("Kunal", "Singh", 5, NULL),
 ("Malia", "Brown", 6, 5),
-("Sarah", "Lourd", 7),
+("Sarah", "Lourd", 7, NULL),
 ("Tom", "Allen", 8, 7)
