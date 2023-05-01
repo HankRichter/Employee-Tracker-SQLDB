e204693ba91968efec2bfa58a35dc4f740ab3a46
Hank Richter <120127039+HankRichter@users.noreply.github.com>
2023-04-30 20:16:59 -0400

    Changed table names and added FOREIGN KEYS and REFRENCES.

--- db/schema.sql
+++ db/schema.sql
@@ -8,17 +8,26 @@ CREATE TABLE departments (
     department_name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role(
+CREATE TABLE roles(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL
-    department_id INT,
+    salary DECIMAL,
+    departments_id INT,
+    FOREIGN KEY (departments_id)
+    REFERENCES departments(id)
+    ON DELETE SET NULL
 );
 
-CREATE TABLE employee(
+CREATE TABLE employees(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT,
+    roles_id INT,
+    FOREIGN KEY (roles_id)
+    REFERENCES roles(id)
+    ON DELETE SET NULL,
     manager_id INT,
+    FOREIGN KEY (manager_id)
+    REFERENCES employees(id)
+    ON DELETE SET NULL
 );
